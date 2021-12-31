96cbbbb6ba9a6b1207401cd886b25dab39993139
tyler-wadekamper <wadekampert@spu.edu>
2021-12-31 11:16:24 -0800

    Finish button event handlers and keypad methods.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 class Calculator {
     constructor() {
-        this.screen = new Screen(this);
+        // this.screen = new Screen(this);
         this.engine = new Engine(this);
         this.keypad = new Keypad(this);
-        this.state = new State(this);
+        // this.state = new State(this);
     }
 
     handleNumber(number) {
@@ -48,6 +48,38 @@ class Keypad {
         this.calculator = calculator;
         this.buttonCreator = new ButtonCreator(this);
         this.buttonCreator.createButtons();
+    }
+
+    sendNumber(buttonNumber) {
+        this.calculator.handleNumber(buttonNumber);
+    }
+
+    sendAdd() {
+        this.calculator.handleAdd();
+    }
+
+    sendSubtract() {
+        this.calculator.handleSubtract();
+    }
+
+    sendMultiply() {
+        this.calculator.handleMultiply();
+    }
+
+    sendDivide() {
+        this.calculator.handleDivide();
+    }
+
+    sendBack() {
+        this.calculator.handleBack();
+    }
+
+    sendClear() {
+        this.calculator.handleClear();
+    }
+
+    sendSubmit() {
+        this.calculator.handleSubmit();
     }
 }
 
@@ -112,11 +144,7 @@ class KeypadButton {
     constructor(keypad, element) {
         this.keypad = keypad;
         this.element = element;
-        this.element.addEventListener('mousedown', this.handleMouseDown);
-    }
-
-    handleMouseDown(event) {
-        console.log(this);
+        this.element.addEventListener('mousedown', this.handleMouseDown.bind(this));
     }
 }
 
@@ -124,11 +152,20 @@ class NumberButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
     }
+    
+    handleMouseDown() {
+        let buttonNumber = this.element.dataset.value;
+        this.keypad.sendNumber(buttonNumber);
+    }
 }
 
 class AddButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
+    }
+
+    handleMouseDown() {
+        this.keypad.sendAdd();
     }
 }
 
@@ -136,11 +173,19 @@ class SubtractButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
     }
+
+    handleMouseDown() {
+        this.keypad.sendSubtract();
+    }
 }
 
 class DivideButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
+    }
+
+    handleMouseDown() {
+        this.keypad.sendDivide();
     }
 }
 
@@ -148,11 +193,19 @@ class MultiplyButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
     }
+
+    handleMouseDown() {
+        this.keypad.sendMultiply();
+    }
 }
 
 class BackButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
+    }
+
+    handleMouseDown() {
+        this.keypad.sendBack();
     }
 }
 
@@ -160,11 +213,19 @@ class ClearButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
     }
+
+    handleMouseDown() {
+        this.keypad.sendClear();
+    }
 }
 
 class SubmitButton extends KeypadButton {
     constructor(keypad, element) {
         super(keypad, element);
+    }
+
+    handleMouseDown() {
+        this.keypad.sendSubmit();
     }
 }
 
